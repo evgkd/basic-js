6ec42584e865c1652d546651478d2eb95a201139
evgkd <evgkd1993@gmail.com>
2025-11-06 09:54:53 +0300

    feat: solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,35 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(direct = true) {
+    this.direct = direct;
   }
-
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  encrypt(message, key) {
+    return this.#process(message, key, true);
+  }
+  decrypt(message, key) {
+    return this.#process(message, key, false);
+  }
+  #process(message, key, isEncrypt) {
+    const msg = String(message).toUpperCase();
+    const k = String(key).toUpperCase();
+    let res = [];
+    let ki = 0;
+    for (let i = 0; i < msg.length; i++) {
+      const ch = msg[i];
+      if (ch >= 'A' && ch <= 'Z') {
+        const m = ch.charCodeAt(0) - 65;
+        const kch = k[ki % k.length];
+        const kidx = kch.charCodeAt(0) - 65;
+        const outIdx = isEncrypt ? (m + kidx) % 26 : (m - kidx + 26) % 26;
+        res.push(String.fromCharCode(outIdx + 65));
+        ki++;
+      } else {
+        res.push(ch);
+      }
+    }
+    const out = res.join('');
+    return this.direct ? out : out.split('').reverse().join('');
   }
 }
 
